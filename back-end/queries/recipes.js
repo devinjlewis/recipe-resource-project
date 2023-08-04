776bcafa88a5611154b67c81cd3919ffa88dadc1
Nicole Rodriguez <nicolerodriguez@Nicoles-Air.lan>
2023-08-04 13:30:26 -0400

    fixed typo

--- back-end/queries/recipes.js
+++ back-end/queries/recipes.js
@@ -60,7 +60,7 @@ const createRecipe = async (recipe) => {
 const deleteRecipe = async (id) => {
   try {
     const deletedRecipe = await db.any(
-      `DELETE FROM Recipes WHERE id = $1 RETURNING *`,
+      `DELETE FROM recipes WHERE id = $1 RETURNING *`,
       id
     );
 
@@ -69,57 +69,6 @@ const deleteRecipe = async (id) => {
     return e;
   }
 };
-
-// const updateRecipe = async (Recipe, id) => {
-//   try {
-//     const { name, artist, album, time, is_favorite } = Recipe;
-//     const updateFields = {
-//       name,
-//       artist,
-//       album,
-//       time,
-//       is_favorite,
-//     };
-//     console.log(updateFields);
-//     let query = "UPDATE Recipes SET ";
-//     let values = [];
-//     let valueIndex = 1;
-
-//     for (const field in updateFields) {
-//       const value = updateFields[field];
-//       if (value !== null && value !== undefined) {
-//         query += `${field} = $${valueIndex}, `;
-//         values.push(value);
-//         valueIndex++;
-//       }
-//     }
-//     query = query.slice(0, -2);
-
-//     query += ` WHERE id = $${valueIndex++} RETURNING *`;
-
-//     const updatedRecipe = await db.any(query, [...values, id]);
-//     /*
-//         const updatedRecipe = await db.any(
-//             "UPDATE  Recipes SET name = '$1', artist = '$2' , album = '$3',time = '$4', is_favorite=$5 WHERE id=$6 RETURNING *",
-//             [name, artist, album, time, is_favorite, id]
-//         );*/
-
-//     return updatedRecipe;
-//   } catch (e) {
-//     return e;
-//   }
-// };
-// const getAllArtistsOnRecipe = async (id) => {
-//   try {
-//     const recipes = await db.any(
-//       "SELECT a.* FROM collaborations c LEFT JOIN Recipes s ON c.recipe_id=s.id LEFT JOIN artist a ON c.artist_id=a.id WHERE c.Recipe_id=" +
-//         id
-//     );
-//     return recipes;
-//   } catch (error) {
-//     return error;
-//   }
-// };
 
 module.exports = {
   getAllRecipes,
