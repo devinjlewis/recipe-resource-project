befbe505eb26801965b3ef7cd3425f90f8483451
Nicole Rodriguez <nicolerodriguez@Nicoles-Air.lan>
2023-08-07 12:24:31 -0400

    added loading state, and recipe details page

--- front-end/src/components/Home/Home.js
+++ front-end/src/components/Home/Home.js
@@ -1,49 +1,59 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { getAllRecipes } from "../Api/Api";
 
 function Home() {
-    const [recipes, setRecipes] = useState([]);
-    const fetchData = async () => {
-        const recipe = await getAllRecipes();
-        setRecipes(recipe.data);
-    };
-    useEffect(() => {
-        fetchData();
-    }, []);
+  const [recipes, setRecipes] = useState([]);
+  const fetchData = async () => {
+    const recipe = await getAllRecipes();
 
+    setRecipes(recipe.data);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (!recipes) {
     return (
-        <div className="container mt-4">
-            <h1 className="m-5">All Recipes</h1>
-            <div className="row">
-                {recipes.map((recipe) => (
-                    <div className="col-md-4 mb-4" key={recipe.id}>
-                        <div className="card" style={{ width: "18rem" }}>
-                            {/* <img
+      <div className="loader-container">
+        <div className="d-flex justify-content-center m-5 loader ">
+          Loading...
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="container mt-4">
+      <h1 className="m-5">All Recipes</h1>
+      <div className="row">
+        {recipes.map((recipe) => (
+          <div className="col-md-4 mb-4" key={recipe.id}>
+            <div className="card" style={{ width: "18rem" }}>
+              {/* <img
                 src={recipe.image}
                 className="card-img-top"
                 alt={recipe.name}
               /> */}
-                            <div className="card-body">
-                                <h5 className="card-title">{recipe.name}</h5>
-                                <p className="card-text">
-                                    Category: {recipe.origin}
-                                </p>
-                                <p className="card-text">
-                                    Cook Time: {recipe.cook_time} minutes
-                                </p>
-                                <a
-                                    href="#"
-                                    className="btn btn-success d-flex justify-content-center"
-                                >
-                                    View More
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                ))}
+              <div className="card-body">
+                <h5 className="card-title">{recipe.name}</h5>
+                <p className="card-text">Category: {recipe.origin}</p>
+                <p className="card-text">
+                  Cook Time: {recipe.cook_time} minutes
+                </p>
+                <Link
+                  to={`/recipes/${recipe.id}`}
+                  className="btn btn-success d-flex justify-content-center"
+                >
+                  View More
+                </Link>
+              </div>
             </div>
-        </div>
-    );
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
