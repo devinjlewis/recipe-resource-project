6eec05917bdad333be159b0ebed22c5b3a452d57
Nicole Rodriguez <nicolerodriguez@Nicoles-Air.lan>
2023-08-07 23:49:06 -0400

    styled using bootstrap

--- front-end/src/components/Recipe/Recipe.js
+++ front-end/src/components/Recipe/Recipe.js
@@ -31,13 +31,47 @@ function Recipe() {
   }
   return (
     <div className="container mt-4">
-      <h1 className="m-5">Recipe Details</h1>
-      <div className="card" style={{ width: "18rem" }}>
-        <div className="card-body">
-          <h5 className="card-title">{recipe.name}</h5>
-          <p className="card-text">Category: {recipe.origin}</p>
-          <p className="card-text">Cook Time: {recipe.cook_time} minutes</p>
+      <h1 className=" text-center mb-5 m-5">{recipe.name}</h1>
+
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card p-4" style={{ maxWidth: "900px" }}>
+            <div className="card-body">
+              <p className="card-text">
+                <span className="fw-bold">Ingredients: </span>
+                {recipe.ingredients}
+              </p>
+              <p className="card-text">
+                <span className="fw-bold">Instructions:</span>{" "}
+                {recipe.instructions}
+              </p>
+              <p className="card-text">
+                <span className="fw-bold">Origin:</span> {recipe.origin}
+              </p>
+              <p className="card-text">
+                <span className="fw-bold">Serving Size:</span>
+                {recipe.serving_size}
+              </p>
+              <p className="card-text">
+                <span className="fw-bold">Cook Time:</span> {recipe.cook_time}
+                minutes
+              </p>
+              <p className="card-text">
+                <span className="fw-bold">Prep Time:</span> {recipe.prep_time}
+                minutes
+              </p>
+              <p className="card-text">
+                <span className="fw-bold"> Total Time: </span>
+                {recipe.prep_time + recipe.cook_time} minutes
+              </p>
+            </div>
+          </div>
         </div>
+      </div>
+
+      <div className="d-flex justify-content-center mt-4">
+        <button className="btn btn-primary mx-2">Edit</button>
+        <button className="btn btn-danger mx-2">Delete</button>
       </div>
     </div>
   );
