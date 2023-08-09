c1b7d071f9055a748e98617fc59f36ff760a4edd
Nicole Rodriguez <nicolerodriguez@Nicoles-Air.lan>
2023-08-09 14:58:01 -0400

    styling and cleaning up code

--- front-end/src/components/Categories/Categories.js
+++ front-end/src/components/Categories/Categories.js
@@ -3,57 +3,57 @@ import { useEffect, useState } from "react";
 import { getAllCategories } from "../Api/Api";
 import "./Categories.css";
 function Categories() {
-    const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-    useEffect(() => {
-        fetchCategories();
-    }, []);
+  useEffect(() => {
+    fetchCategories();
+  }, []);
 
-    const fetchCategories = async () => {
-        try {
-            //const response = await axios.get(url + "/bookmarks/" + id);
-            const response = await getAllCategories();
-            const data = response.data;
-            setCategories(data);
-        } catch (error) {
-            console.error("Error fetching categories:", error);
-        }
-    };
-
-    if (!categories.length) {
-        return (
-            <div className="loader-container">
-                <div className="d-flex justify-content-center m-5 loader ">
-                    Loading...
-                </div>
-            </div>
-        );
+  const fetchCategories = async () => {
+    try {
+      //const response = await axios.get(url + "/bookmarks/" + id);
+      const response = await getAllCategories();
+      const data = response.data;
+      setCategories(data);
+    } catch (error) {
+      console.error("Error fetching categories:", error);
     }
+  };
 
+  if (!categories.length) {
     return (
-        <div className="row">
-            <div className="col">
-                <div className="list-group sidebar">
-                    <div class="hover">
-                        <Link to="/">All Recipes</Link>
-                    </div>
-                    {categories &&
-                        categories.map((category) => {
-                            return (
-                                <div class="hover">
-                                    <Link
-                                        key={category.id}
-                                        to={`/recipes/categories/${category.id}`}
-                                    >
-                                        {category.category_name}
-                                    </Link>
-                                </div>
-                            );
-                        })}
-                </div>
-            </div>
+      <div className="loader-container">
+        <div className="d-flex justify-content-center m-5 loader ">
+          Loading...
         </div>
+      </div>
     );
+  }
+
+  return (
+    <div className="row">
+      <div className="col">
+        <div className="list-group sidebar">
+          <div class="hover my-4">
+            <Link to="/">All Recipes</Link>
+          </div>
+          {categories &&
+            categories.map((category) => {
+              return (
+                <div class="hover my-4">
+                  <Link
+                    key={category.id}
+                    to={`/recipes/categories/${category.id}`}
+                  >
+                    {category.category_name}
+                  </Link>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Categories;
